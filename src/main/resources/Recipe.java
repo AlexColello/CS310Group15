a0390427fd783adce39b386cd8dd7c43e1858da9
AlexColello <acolello@usc.edu>
2019-03-01 12:48:18 -0800

    Fixed some build path issues and added Recipe object

--- src/main/resources/Recipe.java
+++ src/main/resources/Recipe.java
@@ -1,8 +1,52 @@
+import java.util.ArrayList;
 
 public class Recipe {
 
-	public Recipe() {
-		
+	private String name = "";
+	private String pictureUrl = "";
+	private double prepTime = -1;
+	private double cookTime = -1;
+	private ArrayList<String> ingredients;
+	private ArrayList<String> instructions;
+	private double rating = -1;
+	
+	public Recipe(String name, String pictureUrl, double prepTime, double cookTime, ArrayList<String> ingredients,
+			ArrayList<String> instructions, double rating) {
+		this.name = name;
+		this.pictureUrl = pictureUrl;
+		this.prepTime = prepTime;
+		this.cookTime = cookTime;
+		this.ingredients = ingredients;
+		this.instructions = instructions;
+		this.rating = rating;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPictureUrl() {
+		return pictureUrl;
+	}
+
+	public double getPrepTime() {
+		return prepTime;
+	}
+
+	public double getCookTime() {
+		return cookTime;
+	}
+
+	public ArrayList<String> getIngredients() {
+		return ingredients;
+	}
+
+	public ArrayList<String> getInstructions() {
+		return instructions;
+	}
+
+	public double getRating() {
+		return rating;
 	}
 	
 }
