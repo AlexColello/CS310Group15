2e304939d5a6465dffded3602a35f4ac350e97a2
Jiho Kim <captainhoji@naver.com>
2019-03-03 19:52:51 -0800

    component file changed

--- src/main/resources/api/AccessYelpAPI.java
+++ src/main/resources/api/AccessYelpAPI.java
@@ -172,12 +172,11 @@ public class AccessYelpAPI {
 				ex.printStackTrace();
 			}
 		}
-		
 		return resultsAL;		//returns ArrayList of restaurant objects
 	}
 	
 	//Uncomment main method and run as Java Application to test
-	
+	/*
 	public static void main(String[] args) {
 		int count = 60;
 		Vector<Restaurant> arr = AccessYelpAPI.YelpRestaurantSearch("mexican", count);
@@ -187,5 +186,5 @@ public class AccessYelpAPI {
 			System.out.println("driveTime: " + arr.get(i).getDrivingTime());
 		}
 	}
-	
+	*/
 }
