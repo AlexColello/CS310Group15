31547903cc9f9b71c9c929770c65cc92f9e7d88f
AlexColello <acolello@usc.edu>
2019-03-03 19:11:44 -0800

    Added GoogleDirections class

---
+++ src/main/resources/api/GoogleDirections.java
@@ -0,0 +1,15 @@
+package api;
+
+public class GoogleDirections {
+	
+	private final String apiKey = "AIzaSyCVTgss5eiM_qBXwsnz_UgVK4d-FWiXZIM";
+	
+	public static double getDrivingTime(double startLatitude, double startLongitude, double endLatitude, double endLongitude) {
+		
+		
+		
+		
+		return 0;
+	}
+
+}
