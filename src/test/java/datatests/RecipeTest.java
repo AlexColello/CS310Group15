0a653839943bce5ce9a2473bd00fdb7ba7da09cb
Alex Colello <acolello@usc.edu>
2019-03-07 18:01:59 -0800

    Finished Data tests

--- src/test/java/datatests/RecipeTest.java
+++ src/test/java/datatests/RecipeTest.java
@@ -1,12 +1,16 @@
 package datatests;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Vector;
 
 import org.junit.Test;
 
 import data.Recipe;
+import data.Restaurant;
 
 public class RecipeTest {
 
@@ -74,6 +78,40 @@ public class RecipeTest {
 		assertEquals(recipe3, recipes.get(2));
 		assertEquals(recipe2, recipes.get(3));
 		
+	}
+	
+	@Test
+	public void testEquals() {
+		String name = "Good Food";
+		String pictureUrl = "http://www.todayifoundout.com/wp-content/uploads/2017/11/rick-astley.png";
+		double prepTime = 10;
+		double cookTime = 25;
+		ArrayList<String> ingredients = new ArrayList<String>();
+		ingredients.add("1 teaspoon ground ginger");
+		ingredients.add("1 rack of lamb");
+		ArrayList<String> instructions = new ArrayList<String>();
+		instructions.add("Throw in a pan.");
+		instructions.add("Cook until done.");
+		double rating = 4.5;
+		
+		Recipe recipe1 = new Recipe(name, pictureUrl, prepTime, cookTime, ingredients, instructions, rating);
+		Recipe recipe2 = new Recipe("The Best " + name, pictureUrl, prepTime + 5, cookTime, ingredients, instructions, rating);
+
+		ArrayList<Recipe> recipes = new ArrayList<Recipe>();
+		recipes.add(recipe1);
+
+		assertTrue(recipes.contains(recipe1));
+		assertFalse(recipes.contains(recipe2));
+		
+		String websiteUrl = "https://www.mcdonalds.com/us/en-us.html";
+		int price = 1;
+		String address = "Everywhere";
+		String phoneNumber = "(123)456-7890";
+		int drivingTime = 5;
+		Restaurant restaurant1 = new Restaurant(name, websiteUrl, price, address, phoneNumber, rating, drivingTime);
+		
+		assertFalse(recipe1.equals(restaurant1));
+		assertFalse(recipes.contains(restaurant1));
 		
 	}
 
